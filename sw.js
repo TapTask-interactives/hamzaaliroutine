1d67c6af570daf7df88a2bb2677e1051d78686e3
TapTask-interactives <salma.khairallah.dm21@gmail.com>
2026-09-18 21:43:02 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'routine-hamza-ali-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.webmanifest',
+  './favicon.png',
+  './icon-192.png',
+  './icon-512.png',
+  './assets/children.jpg',
+  './assets/morning-bg.png',
+  './assets/nightly-bg.png',
+  './assets/study-bg.png',
+  './assets/bismillah.mp3',
+  './assets/fact-popup.mp3',
+  './assets/task-done.mp3'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
